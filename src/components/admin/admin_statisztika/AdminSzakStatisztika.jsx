6a665fdd08d39849d01119cb48a68e655f6fbe78
bajari.gergo <gergob10@gmail.com>
2025-03-13 15:33:30 +0100

    Kártyák elhelyezése, helyes adatmegjelentítés

--- src/components/admin/admin_statisztika/AdminSzakStatisztika.jsx
+++ src/components/admin/admin_statisztika/AdminSzakStatisztika.jsx
@@ -3,6 +3,14 @@ import { PieChart } from "@mui/x-charts/PieChart";
 import { AdminSzakStatisztikaContext } from "../../../context/admin/AdminSzakStatisztikaContext";
 import { map } from "zod";
 import { BarChart } from '@mui/x-charts/BarChart';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+import SaveIcon from '@mui/icons-material/Save';
 
 
 function AdminSzakStatisztika() {
@@ -11,83 +19,144 @@ function AdminSzakStatisztika() {
   const { jelentkezokSzamaTagozatraSzakokra } = useContext(AdminSzakStatisztikaContext)
 
   console.log(nappaliEsti);
+  const tagozatSzakData = [
+    {
+      label: 'Nappali',
+      stack: 'A',  // A nappali adatok a stack A-ba kerülnek
+      data: jelentkezokSzamaTagozatraSzakokra.map((item) => item.nappali)
+    },
+    {
+      label: 'Esti',
+      stack: 'A',  // Az esti adatok szintén az A stack-en
+      data: jelentkezokSzamaTagozatraSzakokra.map((item) => item.esti)
+    },
+    {
+      label: 'Összesen',
+      data: jelentkezokSzamaTagozatraSzakokra.map((item) => item.osszesen)
+    }
+  ];
+  const tagozatSzakxAxisData =jelentkezokSzamaTagozatraSzakokra.map((item) => item.elnevezes);
   return (
-    <div className="pt-24 flex justify-center">
-      <PieChart
-      margin={{ top: 50, bottom: 50, left: 50, right:50 }}
-        series={[
-          {
-            data: [
-              { id: 0, value: nappaliEsti.nappali, label: "nappali" },
-              { id: 1, value: nappaliEsti.esti, label: "esti" },
-            ],
-            highlightScope: { fade: "global", highlight: "item" },
-            faded: { innerRadius: 30, additionalRadius: -80, color: "gray" },
-          },
-        ]}
-        width={800}
-        height={400}
-        slotProps={{
-          legend: {
-            direction: 'row',
-            position: { vertical: 'bottom', horizontal: 'middle' },
-            padding: 2,
-          },
-        }
-        }
-      />
+    <div className="pt-24 flex justify-center m-5 flex-wrap gap-20">
+      <Card sx={{ maxWidth: 700 }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Tagozantonkénti jelentkezők száma
+            </Typography>
+            <PieChart
+              margin={{ top: 50, bottom: 50, left: 50, right: 75 }}
+              series={[
+                {
+                  data: [
+                    { id: 0, value: nappaliEsti.nappali, label: "nappali" },
+                    { id: 1, value: nappaliEsti.esti, label: "esti" },
+                  ],
+                  highlightScope: { fade: "global", highlight: "item" },
+                  faded: { innerRadius: 30, additionalRadius: -80, color: "gray" },
+                },
+              ]}
+              width={700}
+              height={400}
+              slotProps={{
+                legend: {
+                  direction: 'row',
+                  position: { vertical: 'bottom', horizontal: 'middle' },
+                  padding: 2,
+                },
+              }
+              }
+            />
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<SaveIcon />}
+          >
+            Letöltés
+          </Button>
+        </CardActions>
+      </Card>
+      <Card sx={{ maxWidth: 780 }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Szakonkénti jelentkezők száma
+            </Typography>
+            <PieChart
+              margin={{ top: 20, bottom: 100, left: 50, right: 50 }}
+              series={[
+                {
+                  data: jelentkezokSzamaSzakokra.map((data, index) => ({
+                    id: index,
+                    label: data.elnevezes,
+                    value: data.ennyien,
+                  })),
+                  highlightScope: { fade: 'global', highlight: 'item' },
+                  faded: { innerRadius: 30, additionalRadius: -80, color: 'gray' },
 
-      <PieChart
-      margin={{ top: 50, bottom: 50, left: 50, right:50 }}
-        series={[
-          {
-            data: jelentkezokSzamaSzakokra.map((data, index) => ({
-              id: index,
-              label: data.elnevezes,
-              value: data.ennyien,
-            })),
-            highlightScope: { fade: 'global', highlight: 'item' },
-            faded: { innerRadius: 30, additionalRadius: -80, color: 'gray' },
+                },
+              ]}
+              width={780}
+              height={400}
+              slotProps={{
+                legend: {
+                  direction: 'row',
+                  position: { vertical: 'bottom', horizontal: 'middle' },
+                  padding: 2,
+                  markGap: 2,
+                  itemGap: 5,
+                },
+              }
+              }
+            />
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<SaveIcon />}
+          >
+            Letöltés
+          </Button>
+        </CardActions>
+      </Card>
 
-          },
-        ]}
-        width={800}
-        height={400}
-        slotProps={{
-          legend: {
-            direction: 'row',
-            position: { vertical: 'bottom', horizontal: 'middle' },
-            padding: 2,
-          },
-        }
-        }
-      />
-      <BarChart
-        series={[
-          {
-            data: [jelentkezokSzamaTagozatraSzakokra.map((data) => ({
-              value: data.nappali
-            }))], stack: 'A', label: 'Nappali'
-          },
-          {
-            data: [jelentkezokSzamaTagozatraSzakokra.map((data) => ({
-              value: data.esti
-            }))], stack: 'A', label: 'Esti'
-          },
-          {
-            data: [jelentkezokSzamaTagozatraSzakokra.map((data) => ({
-              value: data.osszesen
-            }))], label: 'Összesen'
-          },
-        ]}
-        xAxis={[{
-          scaleType: 'band', data: jelentkezokSzamaTagozatraSzakokra.map((data) => ({
-            value: data.elnevezes
-          }))
-        }]}
-        width={600}
-        height={350}
-      />
+
+      <Card sx={{ maxWidth: '100%' }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Oszlopdiagramm tagozatonkénti jelentkezésekhez
+            </Typography>
+            <BarChart
+              series={tagozatSzakData}
+              xAxis={[{
+                scaleType: 'band', data: tagozatSzakxAxisData}]}
+              width={1600}
+              height={350}
+            />
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<SaveIcon />}
+          >
+            Letöltés
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
